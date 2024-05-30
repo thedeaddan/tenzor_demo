5fb4bf19f02239dfb0a172b2050173ae393e6011
Даниил <thedeaddan@mail.ru>
2024-05-30 13:16:09 +0300

    Убран лог в файл, убран репорт в HTML

--- conftest.py
+++ conftest.py
@@ -1,9 +1,2 @@
-import pytest
-import logging
 
-logging.basicConfig(filename='logs/test_log.log', level=logging.INFO)
 
-@pytest.hookimpl(tryfirst=True)
-def pytest_configure(config):
-    config.option.htmlpath = 'report.html'
-    config.option.self_contained_html = True
